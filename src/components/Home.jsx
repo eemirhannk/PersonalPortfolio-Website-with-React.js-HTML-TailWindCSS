1264e11bf890367ab8fa4d5dd3aae3bfb267f527
eemirhannk <eemirhannk@gmail.com>
2023-03-29 02:17:27 +0300

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
+import Typed from 'typed.js'
+
+
 
 const Home = () => {
+    // Create reference to store the DOM element containing the animation
+    const el = React.useRef(null);
+  
+    React.useEffect(() => {
+      const typed = new Typed(el.current, {
+        strings: ['Frontend Developer.', 'UI Designer.','CS Student.'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+      });
+  
+      return () => {
+        // Destroy Typed instance during cleanup to stop animation
+        typed.destroy();
+      };
+    }, []);
   return (
     <section className='min-h-screen pt-40 px-[9%] pb-8 flex justify-between items-center max-[991px]:pt-40 max-[991px]:px-[3%] max-[991px]:pb-8 max-[768px]:flex-col' id='home'>
     <div className='home-content'>
         <h3 className='text-[3.2rem] font-bold max-[768px]:text-[2.6rem]'>Hello, It's Me</h3>
         <h1 className='text-[5.6rem] font-bold leading-[1.3] max-[768px]:text-[5rem]'>Emirhan Koç</h1>
-        <h3 className='text-[3.2rem] font-bold mb-4 max-[768px]:text-[2.6rem]'>And I'm a <span className='text-blue'>Frontend Developer</span></h3>
+        <h3 className='text-[3.2rem] font-bold mb-4 max-[768px]:text-[2.6rem]'>And I'm a <span ref={el} className='text-blue'>Frontend Developer.</span></h3>
         <p className='text-[1.6rem]'>I'm 20 years old Computer Engineering Student of Kocaeli University.</p>
-        <div>
+        <div className='social-media'>
           {/* Aşağıdaki linklere box shadow koyulacak tailwind kullanarak başaramadım. */}
             <a href="https://www.instagram.com/eemirkoc" target='_blank' className='inline-flex justify-center items-center w-16 h-16 bg-transparent border-[.2rem] border-solid border-blue rounded-[50%] text-[2rem] text-blue mt-12 mr-6 mb-12 ml-0 ease-linear duration-[.3s] hover:bg-blue hover:text-grey hover:shadow-first'><i class='bx bxl-instagram-alt'></i></a>
             <a href="https://www.twitter.com/eemirkocc" target='_blank' className='inline-flex justify-center items-center w-16 h-16 bg-transparent border-[.2rem] border-solid border-blue rounded-[50%] text-[2rem] text-blue mt-12 mr-6 mb-12 ml-0 ease-linear duration-[.3s] hover:bg-blue hover:text-grey hover:shadow-first'><i class='bx bxl-twitter' ></i></a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className=''>
-      <a href="#home" className='active text-[1.7rem] text-white no-underline font-medium ml-16 ease-linear duration-[.3s] hover:text-blue'>Home</a>
-      <a href="#about" className='text-[1.7rem] text-white no-underline font-medium ml-16 ease-linear duration-[.3s] hover:text-blue'>About</a>
-      <a href="#skills" className='text-[1.7rem] text-white no-underline font-medium ml-16 ease-linear duration-[.3s] hover:text-blue'>Skills</a>
-      <a href="#portfolio" className='text-[1.7rem] text-white no-underline font-medium ml-16 ease-linear duration-[.3s] hover:text-blue'>Projects</a>
-      <a href="#contact" className='text-[1.7rem] text-white no-underline font-medium ml-16 ease-linear duration-[.3s] hover:text-blue'>Contact</a>
+    <nav className='navbar'>
+      <a href="#home" className='active inline-block text-[1.7rem] text-white no-underline font-medium ml-16 ease-linear duration-[.3s] hover:text-blue'>Home</a>
+      <a href="#about"  className='inline-block text-[1.7rem] text-white no-underline font-medium ml-16 ease-linear duration-[.3s] hover:text-blue'>About</a>
+      <a href="#skills"  className='inline-block text-[1.7rem] text-white no-underline font-medium ml-16 ease-linear duration-[.3s] hover:text-blue'>Skills</a>
+      <a href="#portfolio"  className='inline-block text-[1.7rem] text-white no-underline font-medium ml-16 ease-linear duration-[.3s] hover:text-blue'>Projects</a>
+      <a href="#contact"  className='inline-block text-[1.7rem] text-white no-underline font-medium ml-16 ease-linear duration-[.3s] hover:text-blue'>Contact</a>
     </nav>
   )
 }
