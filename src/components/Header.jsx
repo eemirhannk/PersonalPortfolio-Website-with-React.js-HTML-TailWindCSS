07ba0d42d8f36777b96eaa38cbb899399819d815
Emirhan Koç <eemirhannk@gmail.com>
2023-04-03 21:22:59 +0300

    feat: basic edits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <header className='fixed top-0 left-0 w-full px-[9%] py-[2rem] flex justify-between items-center z-100 bg-dark max-[991px]:py-8 max-[991px]:px-[3%] '>
+    <header className='fixed z-[1000] top-0 left-0 w-full px-[9%] py-[2rem] flex justify-between items-center z-100 bg-dark max-[991px]:py-8 max-[991px]:px-[3%] '>
     <a href="#home" className='animate-[slideRight_1s_ease_forwards] text-[2.5rem] text-white font-semibold cursor-pointer'>Portfolio</a>
     <i className='text-[3.6rem] text-white hidden bx bx-menu max-[768px]:block cursor-pointer' id='menu-icon'></i>
 
